2d42a9571efa2c78105b05f05b28c95be3848172
Nazrawi <nazrawiijobs@gmail.com>
2025-12-20 00:23:56 +0300

    Initial backup: nvim, mpv, viu

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,20 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- This file is for your own custom keymaps
+-- See `:help vim.keymap.set()`
+local Util = require("lazyvim.util")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    -- Open Markdown preview
+    vim.keymap.set("n", "<leader>!p", "<cmd>Glow<CR>", { desc = "Markdown Preview" })
+  end,
+})
+
+-- AI/CodeCompanion keymaps
+vim.keymap.set({ "n", "v" }, "<leader>ac", "<cmd>CodeCompanion<CR>", { desc = "CodeCompanion Toggle" })
+vim.keymap.set({ "n", "v" }, "<leader>aa", "<cmd>CodeCompanionActions<CR>", { desc = "CodeCompanion Actions" })
+vim.keymap.set("n", "<leader>aC", "<cmd>CodeCompanionChatToggle<CR>", { desc = "CodeCompanion Chat Toggle" })
---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,22 @@
+-- return {
+--   -- Markdown previewer
+--   {
+--     "ellisonleao/glow.nvim",
+--     config = function()
+--       require("glow").setup({})
+--     end,
+--     cmd = "Glow",
+--   },
+-- }
+--
+
+return {
+  -- Markdown previewer
+  {
+    "ellisonleao/glow.nvim",
+    config = function()
+      require("glow").setup({})
+    end,
+    cmd = "Glow",
+  },
+}
